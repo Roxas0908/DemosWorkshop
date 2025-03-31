f9a6854c49bd82d1946192dc2dac538c9750e300
liumingsong <liumingsong@baijia.com>
2025-03-31 13:47:20 +0800

    [feat] 下拉二楼效果初步demo

---
+++ app/src/main/java/com/example/demoworkshop/smartrefrshlayout/SecondFloorDemoActivity.kt
@@ -0,0 +1,51 @@
+package com.example.demoworkshop.smartrefrshlayout
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.demoworkshop.R
+import com.example.demoworkshop.databinding.ActivitySecondFloorDemoBinding
+
+
+class SecondFloorDemoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondFloorDemoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondFloorDemoBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        initView()
+
+        binding.refreshLayout.setOnRefreshListener {
+            Toast.makeText(applicationContext, "下拉刷新", Toast.LENGTH_SHORT).show()
+            binding.refreshLayout.finishRefresh(2000)
+        }
+
+        binding.refreshLayout.setOnLoadMoreListener {
+            Toast.makeText(applicationContext, "上拉加载", Toast.LENGTH_SHORT).show()
+            binding.refreshLayout.finishLoadMore(2000)
+        }
+
+        binding.header.setOnTwoLevelListener {
+            Toast.makeText(applicationContext, "打开二楼", Toast.LENGTH_SHORT).show()
+//            findViewById<View>(R.id.second_floor_content).animate().alpha(1f).setDuration(2000)
+            true
+        }
+
+        binding.tvReturn.setOnClickListener {
+            binding.header.finishTwoLevel()
+        }
+        binding.secondFloor.setOnClickListener {
+            binding.header.finishTwoLevel()
+        }
+    }
+
+    private fun initView() {
+
+
+    }
+}
+
